1fa25fa07ad20093c2264cc57105ac886cc4dad2
jang sung jo <jjag051@hanmail.net>
2021-11-11 16:19:22 +0900

    product end

--- js/main.js
+++ js/main.js
@@ -9,4 +9,15 @@ const mainVisualSlider = new Swiper("#mainVisual", {
     clickable: true,
   },
 });
+
+const productSlider = new Swiper("#product .list", {
+  speed: 600,
+  slidesPerView: 3, //화면에 보여지는 갯수
+  slidesPerGroup: 3, //묶음
+  navigation: {
+    prevEl: "#product .prev",
+    nextEl: "#product .next",
+  },
+});
+
 //f(x,y) = x+y*3; f(3,5) = 18
