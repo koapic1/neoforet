4f44db8d85e0cb8b425cfd2efbb3a97f5b5e07e0
koapic1 <koapic1@gmail.com>
2021-11-24 14:38:51 +0900

    slogan gsap splitting 효과 추가

--- pc/js/main.js
+++ pc/js/main.js
@@ -1,23 +1,28 @@
+Splitting();
+
 const mainVisualSlider = new Swiper("#mainVisual", {
-  autoplay: true,
-  speed: 1000,
-  effect: "fade",
-  loop: true,
-  pagination: {
-    el: "#mainVisual .pagination",
-    type: "bullets",
-    clickable: true,
-  },
+    autoplay: true,
+    speed: 1000,
+    effect: "fade",
+    loop: true,
+    pagination: {
+        el: "#mainVisual .pagination",
+        type: "bullets",
+        clickable: true,
+    },
 });
 
 const productSlider = new Swiper("#product .list", {
-  speed: 600,
-  slidesPerView: 3, //화면에 보여지는 갯수
-  slidesPerGroup: 3, //묶음
-  navigation: {
-    prevEl: "#product .prev",
-    nextEl: "#product .next",
-  },
+    speed: 600,
+    slidesPerView: 3, //화면에 보여지는 갯수
+    slidesPerGroup: 3, //묶음
+    navigation: {
+        prevEl: "#product .prev",
+        nextEl: "#product .next",
+    },
 });
+
+gsap.from("#mainVisual .slogan .main .char", { opacity: 0, x: 150, ease: "slow", dutation: 2, stagger: 0.1 });
+gsap.from("#mainVisual .slogan .sub .char", { opacity: 0, y: 150, ease: "back", dutation: 1, delay: 1.8, stagger: 0.1 });
 
 //f(x,y) = x+y*3; f(3,5) = 18
